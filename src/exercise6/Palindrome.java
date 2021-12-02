f9caf4da2cd201486ee8a3bcf3ef34fd653f2e96
borislavgenov <>
2021-12-02 20:47:26 +0000

    test1

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -1,9 +1,25 @@
 package exercise6;
-
 import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int m, d, r=0;
+        boolean palindrome = false;
+        int n = input.nextInt();
+        m = n;
+        
+        while (m != 0){
+          d = m % 10;
+          r = r * 10 + d;
+          m = m / 10;
+        }
+
+      if(r == n){
+        palindrome = true;
+        System.out.println("Palindrome: " + palindrome);
+      }else
+      System.out.println("Palindrome: " + palindrome);
+
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -1,9 +1,42 @@
 package exercise6;
-
 import java.util.Scanner;
 
 public class PasswordCheck {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+      Scanner input = new Scanner(System.in);
+      String realpass = "belot123";
+      String password;
+ 
+    boolean passvalid = false;
+    do{
+      System.out.println("Password: ");
+      password = input.nextLine();
+
+          if(password.length() < 8){
+            continue;
+          }
+          boolean allLettersorDigits = true;
+          int digitcount = 0;
+          for (int index = 0; index<password.length(); index++){
+            char symbol = password.charAt(index);
+            boolean letterordigit = Character.isLetterOrDigit(symbol);
+            if(!letterordigit){
+              allLettersorDigits = false;
+              break;
+            }
+            boolean digit = Character.isDigit(symbol);
+            if(digit){
+              digitcount++;
+            }
+          }
+
+        if (allLettersorDigits && digitcount >= 2){
+          passvalid = true;
+        }
+
+
+    }while (!passvalid);
+
+System.out.println("Password valid!");
     }
 }
